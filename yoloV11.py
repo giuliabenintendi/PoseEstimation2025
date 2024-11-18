64fc398bcabe5d0dd488c9cdd7b41086b96ba25b
Giulia <g.benintendi@studenti.unibs.it>
2024-11-18 18:09:36 +0100

    requirements txt e primo commit

---
+++ yoloV11.py
@@ -0,0 +1,24 @@
+# PER USARE QUESTO SCRIPT USARE CONDA yolov8
+
+from ultralytics import YOLO
+import cv2
+
+# Load the YOLOv11 pose model
+model = YOLO("yolo11n-pose.pt")
+
+# Set device (optional)
+device = 'mps'  # Use 'cpu', 'cuda', or 'mps' for Apple Silicon
+
+# Open a live webcam feed
+cap = cv2.VideoCapture(1)
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    # Set show=True to display the results
+    results = model.predict(frame, show=True)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
